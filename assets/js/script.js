e53e0567900f089fefe9e7919e8775e57387b51a
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-02-03 15:37:07 -0330

    Uv Index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
+// Variable Declaration
 var searchFormEl = document.querySelector("#user-form");
 var searchInputEl = document.querySelector("#city-search");
 var currentWeatherEl = document.querySelector("#current-weather");
 var futureWeatherEl = document.querySelector("#future-weather");
+
+// Variable to store city searches
+var citySearch="";
+var savedCities=[];
+
+// Variable for API key
+var APIkey = "e5755677cedd310e2759e54b55933d69";
+var cityLon;
+var cityLat;
 
 // Function to capture users search criteria and resets the input field 
 var searchFormSubmitHandler = function (event) {
     event.preventDefault();
 
     // get value from input element
-    var citySearch = searchInputEl.value.trim();
+    citySearch = searchInputEl.value.trim();
 
     if (citySearch) {
-        getCurrentCityWeatherData(citySearch);
+        getCityWeatherData(citySearch);
         // clear old content
         currentWeatherEl.textContent = "";
         searchInputEl.value = "";
@@ -19,13 +29,15 @@ var searchFormSubmitHandler = function (event) {
     else {
         alert("Please enter a valid city.") // change from alert to modal 
     }
+
 };
 
 // Function to fetch current weather conditions for users input
-var getCurrentCityWeatherData = function (citySearch) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=e5755677cedd310e2759e54b55933d69";
+var getCityWeatherData = function (citySearch) {
+    // OpenWeather API call to retrieve data from server
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIkey;
 
-    // make a request to the url
+    // make a request to the apiUrl
     fetch(apiUrl)
         .then(function (response) {
             // request was successful
@@ -71,11 +83,11 @@ var displayCurrentWeather = function (currentWeather) {
     var currentHum = document.createElement("p");
     currentHum.textContent = "Humidity: " + currentWeather.main.humidity + "%";
 
-     // UV Index
-    var cityLon = currentWeather.coord.lon;
-    var cityLat = currentWeather.coord.lat;
-    var uvIndexUrl = "http://api.openweathermap.org/v3/uvi/" + cityLat + "," + cityLon + "/?appid=e5755677cedd310e2759e54b55933d69";
-    console.log(uvIndexUrl);
+    // UV Index
+    cityLon = currentWeather.coord.lon;
+    cityLat = currentWeather.coord.lat;
+    // Calls function to get UV Index
+    getUvIndex(cityLon, cityLat);
 
     // Append data to page 
     currentWeatherEl.appendChild(cityName);
@@ -86,8 +98,35 @@ var displayCurrentWeather = function (currentWeather) {
 
 };
 
+// Function to return UV Index
+var getUvIndex = function(uvIndex) {
+        // Get UVIndex API
+        var uvIndexApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + APIkey;
+
+        fetch(uvIndexApi)
+        .then(function(response) {
+            if(response.ok) {
+                response.json()
+                .then(function(data){
+                    var currentUvIndex = document.createElement("p");
+                    var currentUvIndexValue = document.createElement("span");
+                    currentUvIndex.textContent = "UV Index: ";
+                    currentUvIndexValue.textContent = data.current.uvi;
+                    currentUvIndex.appendChild(currentUvIndexValue);
+                    currentWeatherEl.appendChild(currentUvIndex);
+                });;
+            }
+        });
+
+    
+};
+
+// Function to get 5 Day Forecast
+var get5DayForecast = function (citySearch) {
+
+};
 
 
 
-
+// Event Listener on the search form button to trigger the functions that handle displaying the weather data
 searchFormEl.addEventListener("submit", searchFormSubmitHandler);
